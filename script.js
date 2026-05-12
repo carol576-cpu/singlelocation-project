8ff2a52336b29c79f8e7df7136d93b523beb5f65
carol576-cpu <mafongoyacaroline@gmail.com>
2026-05-12 16:19:54 +0200

    added multiple locations

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 function updateClock() {
+  let johannesburgElement = document.querySelector("#johannesburg-time");
+  let johannesburgDateElement = document.querySelector("#johannesburg-date");
+
   let johannesburgTime = moment().tz("Africa/Johannesburg");
 
-  let date = johannesburgTime.format("dddd, MMMM D, YYYY");
+  johannesburgDateElement.innerHTML =
+    johannesburgTime.format("dddd, MMMM D, YYYY");
 
-  let time = johannesburgTime.format("h:mm:ss A");
+  johannesburgElement.innerHTML = johannesburgTime.format("h:mm:ss A");
 
-  document.querySelector("#date").innerHTML = date;
+  let londonElement = document.querySelector("#london-time");
+  let londonDateElement = document.querySelector("#london-date");
 
-  document.querySelector("#time").innerHTML = time;
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("dddd, MMMM D, YYYY");
+
+  londonElement.innerHTML = londonTime.format("h:mm:ss A");
+
+  let newYorkElement = document.querySelector("#newyork-time");
+  let newYorkDateElement = document.querySelector("#newyork-date");
+
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM D, YYYY");
+
+  newYorkElement.innerHTML = newYorkTime.format("h:mm:ss A");
 }
 
 updateClock();
